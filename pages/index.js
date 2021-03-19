005d6fdf955235ad56d41aae210f92b9992d635f
David Malesevic <malesevic.dm@gmail.com>
2021-03-19 09:24:15 +0100

    delazs and responsive

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import ImageGallery from "react-image-gallery";
 import Image from "next/image";
 import React from "react";
 import CountUp from "react-countup";
+import Delayed from "../components/Delayed";
 
 const images = [
   {
@@ -21,42 +22,49 @@ export default function Home() {
       </header>
 
       <article id="third" className="h-full flex flex-row">
-        <p className="z-40 text-white font-mono text-3xl absolute top-1/3 left-2/3">
-          <CountUp end={234390243} duration={12} separator={"'"} />
+        <p className="z-40 text-white font-mono text-6xl absolute top-1/3 left-2/3">
+          <CountUp end={234390243} duration={12} separator={"'"} /> Mal<br></br>
+          einen Monobloc
         </p>
-
+        <Delayed waitBeforeShow={12000}>
+          <div className="z-40 text-white font-mono text-6xl absolute top-3/4 left-2/3">
+            <Link href="/#text">
+              <a>Die geschichte eines Stuhls</a>
+            </Link>
+          </div>
+        </Delayed>
         <video autoPlay muted>
           <source src="monobloc.webm" type="video/webm" />
         </video>
       </article>
-      <article id="third" className="h-full pt-10 flex flex-row">
-        <div className="h-full w-6/12 px-10">
-          <ImageGallery
-            items={images}
-            autoPlay={true}
-            description={images.description}
-            showThumbnails={false}
-            showBullets={true}
-          />
-        </div>
-        <div className="h-full w-6/12 px-10">
-          <h2 className="text-3xl font-sans">
-            Alleine in Italien werden jährlich ca. 10 Millionen Monobloc Stühle
-            hergestellt.
-          </h2>
-          <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-          </p>
-        </div>
-      </article>
+      <Delayed waitBeforeShow={12000}>
+        <article id="text" className="h-full pt-10 flex flex-row sm:flex-col">
+          <div className="h-full w-6/12 sm:w-full px-10">
+            <ImageGallery
+              items={images}
+              autoPlay={true}
+              description={images.description}
+              showThumbnails={false}
+              showBullets={true}
+            />
+          </div>
+          <div className="h-full w-6/12 sm:w-full px-10">
+            <h2 className="text-3xl font-sans">Ein Stuhl für die ganze Welt</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet.
+            </p>
+          </div>
+        </article>
+      </Delayed>
     </div>
   );
 }
