772682bc52fcc6b9b7b822b8ee4d89a3de0395a9
David Malesevic <malesevic.dm@gmail.com>
2021-03-19 11:17:58 +0100

    images

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,33 @@ import Delayed from "../components/Delayed";
 
 const images = [
   {
-    original: "/images/monobloc1.jpg",
+    original: "/images/1.jpg",
     description: "Panton Stuhl von Verner Pantones",
   },
-  { original: "/images/monobloc2.jpg", description: "a chair" },
-  { original: "/images/monobloc3.jpg", description: "a chair" },
+  {
+    original: "/images/2.jpg",
+    description: "Bofinger-Stuhl von Helmut Bätzner",
+  },
+  {
+    original: "/images/3.jpg",
+    description: "Selene Stuhl von Vico Magistretti",
+  },
+  {
+    original: "/images/4.jpg",
+    description: "Fauteuil 300 von Henry Massonnet",
+  },
+  {
+    original: "/images/5.jpg",
+    description: "Resin Chair von Grosfillex",
+  },
+  {
+    original: "/images/6.jpg",
+    description: "111 Navy Chair von Emeco",
+  },
+  {
+    original: "/images/7.jpg",
+    description: "Hemp Hhair von Werner Aisslinger",
+  },
 ];
 
 export default function Home() {
@@ -58,16 +80,49 @@ export default function Home() {
               Ein Stuhl für die ganze Welt
             </h2>
             <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-              invidunt ut labore et dolore magna aliquyam erat, sed diam
-              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-              dolor sit amet.
+              Eine Welt ohne Monobloc ist kaum mehr vorstellbar. Er steht
+              weltweit auf Restaurant Terrassen, in Gärten und sonst überall, wo
+              man eine günstige Sitzmöglichkeit braucht. Wie viele verschiedene
+              Variationen es mittlerweile gibt ist praktisch unmöglich zu sagen.
+            </p>
+            <p>
+              Angefangen hat die Geschichte des Monoblocs in den 1950er Jahren.
+              Durch moderne Fertigungsmethoden war es erstmals möglich einen
+              Stuhl aus einem Stück Plastik herzustellen. Hier hat der Monobloc
+              auch seinen Namen bekommen. Im Spritzgussverfahren wird heisses
+              Plastik in eine Form gepresst und fertig ist der Stuhl.{" "}
+            </p>
+            <p>
+              Zu den ersten in Massenproduktion gefertigten Monobloc Stühlen
+              gehören der Panton (1958-68) von Verner Panton, der Bofinger
+              (1964-68) von Helmut Bätzner und der Selene (1961-68) von Vico
+              Magistretti.{" "}
+            </p>
+            <p>
+              Der weisse Plastikstuhl wie wir ihn heute kennen wurde mit dem
+              Fauteuil 300 erstmals 1972 vom Designer Henry Massonnet erdacht.
+              Seit in den 1980er Jahren die Firma Grosfillex anfing White Resin
+              Chair in grossen Mengen zu Produzieren ist die Verbreitung des
+              Monoblocs nicht mehr aufzuhalten.{" "}
+            </p>
+            <p>
+              Heute dauert die Herstellung eines Stuhls ca. 2 Minuten und kostet
+              3.5 US-Dollar.{" "}
+            </p>
+            <p>
+              So viel Plastik bringt natürlich Probleme mit sich. Monoblocs
+              werden nicht nur in Massen hergestellt, sondern auch entsorgt.
+              Dadurch entstehen Unmengen an Plastikabfall.{" "}
+            </p>
+            <p>
+              Monobloc geht aber auch nachhaltig, so wird z.b. der Emeco 111
+              Navy Chair aus 111 Petflaschen hergestellt. Im Jahr 2012 hat der
+              Designer Werner Aisslinger seinen Hemp Chair vorgestellt, ein
+              Monobloc aus Hanf.
+            </p>
+            <p>
+              Der Monobloc wird sich weiter ausbreiten, es ist nur zu hoffen,
+              dass die Nachhaltigen Varianten möglichst bald überhandnehmen.
             </p>
           </div>
         </article>
